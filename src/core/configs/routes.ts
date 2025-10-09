474383dc236c441ea0a18b438d08433ac4a9d398
Bảo Anh Nguyễn <88286671+alicee-19@users.noreply.github.com>
2025-10-09 12:20:09 +0700

    ui: basket

--- src/core/configs/routes.ts
+++ src/core/configs/routes.ts
@@ -4,6 +4,7 @@ const routes = {
   register: '/register',
   profile: '/profile',
   catalog: '/catalog',
+  basket: '/basket',
   mapDetails: '/catalog/:id',
   checkout: '/checkout',
   logout: '/logout',
