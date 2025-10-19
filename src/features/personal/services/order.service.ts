73205e3999fe504c386953eabd4dfa751b2611b1
Bảo Anh Nguyễn <88286671+alicee-19@users.noreply.github.com>
2025-10-20 01:37:28 +0700

    add: get order by id

--- src/features/personal/services/order.service.ts
+++ src/features/personal/services/order.service.ts
@@ -1,9 +1,15 @@
 import http from '@/common/utils/http';
 
-export const GET_ORDERS_QUERY_KEY = 'getOrders';
+export const GET_ORDERS_QUERY_KEY = 'GET_ORDERS_QUERY_KEY';
+export const GET_ORDER_BY_ID_QUERY_KEY = 'GET_ORDER_BY_ID_QUERY_KEY';
 export const GET_ORDERS_STALE_TIME = 5 * 60 * 1000; // 5 minutes
 
 export const getOrders = () => {
   const result = http.get(`/orders`);
+  return result;
+};
+
+export const getOrderById = (orderId: string) => {
+  const result = http.get(`/orders/${orderId}`);
   return result;
 };
