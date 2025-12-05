096ea59b17b40b82f0fbbb7fe92755d26f53b3b0
Bảo Anh Nguyễn <88286671+alicee-19@users.noreply.github.com>
2025-12-05 21:31:53 +0700

    fix: build error

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -14,7 +14,7 @@ import { motion } from 'framer-motion';
 import { BookOpen, Eye, EyeClosed, GraduationCap, TrendingUp, Users } from 'lucide-react';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 import { LoginFormData, loginSchema } from '../components/schema';
